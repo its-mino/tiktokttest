66031cdfdf4a78acf561b03c9ef06d638919e47a
Brendan Sileo <brendansileo@gmail.com>
2022-02-15 11:51:25 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,17 @@
 from TikTokApi import TikTokApi
 api = TikTokApi()
+
+#Settings
 search_term = "funny"
+num_videos = 30
+
+
 hashtag = api.hashtag(name=search_term)
 authors = []
-for video in hashtag.videos():
+for video in hashtag.videos(count=num_videos):
   authors.append(video.author)
 
 for author in authors:
-  print(author.info_full())
+  data = author.info_full()
+  if data['stats']['followerCount'] >= 250000 and data['stats']['followerCount'] < 1500000:
+    print(data['user']['nickname'], data['stats']['followerCount'])
