0ef8294ae673c8ac4d7e95929a3b6c5374f1f1e6
Brendan Sileo <brendansileo@gmail.com>
2022-02-15 11:44:37 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,5 +2,9 @@ from TikTokApi import TikTokApi
 api = TikTokApi()
 search_term = "funny"
 hashtag = api.hashtag(name=search_term)
+authors = []
 for video in hashtag.videos():
-  print(video)
+  authors.append(video.author)
+
+for author in authors:
+  print(author.info_full())
